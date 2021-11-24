0d0d24f7c350ddccb7d6fda42c5d592b1e556b20
techieton-i <odejobiakin@gmail.com>
2021-11-24 15:16:18 +0100

    e-commerce website mobile first design

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const prdAmt = document.querySelector('.prd-amt');
 const nav = document.querySelector('.nav');
 const burger = document.querySelector('.hamburger');
 const closeBtn = document.querySelector('.menu-close');
+const cartbtn = document.querySelector('.cart');
+const cart = document.querySelector('.cart_list');
+const addItems = document.querySelector('.plus');
+const removeItems = document.querySelector('.minus');
 
 
 
@@ -11,6 +15,10 @@ burger.addEventListener('click', function () {
 
 closeBtn.addEventListener('click', function () {
     nav.classList.remove('open');
+});
+
+cartbtn.addEventListener('click', function () {
+    cart.classList.toggle('open');
 });
 
 prdAmt.textContent = 0;
